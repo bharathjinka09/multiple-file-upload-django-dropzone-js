c5669fdcffe96e8ac895b6dfd3b71071579840cd
bharathjinka09 <bharathjinka09@gmail.com>
2022-01-26 17:56:37 +0530

    Conditional based displaying alert popup message on page load using jquery

--- static/main.js
+++ static/main.js
@@ -187,18 +187,18 @@ overlay.show();
 overlay.appendTo(document.body);
 $('.popup').show();
 $('.close').click(function(){
-$('.popup').hide();
-overlay.appendTo(document.body).remove();
-return false;
+    $('.popup').hide();
+    overlay.appendTo(document.body).remove();
+    return false;
 });
 
 
  
 
 $('.x').click(function(){
-$('.popup').hide();
-overlay.appendTo(document.body).remove();
-return false;
+    $('.popup').hide();
+    overlay.appendTo(document.body).remove();
+    return false;
 });
 });
 
